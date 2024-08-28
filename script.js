af1c7f9135437f5cb0950ab12cbe9211fe8ce8f7
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-28 16:59:47 +0200

    recupero l'elem,ento dal dom, added team array

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // MARKUP side
 // 1.recupero l'elemento dal Dom
+const gridIndex = document.getElementById('gridTeam');
 
 //2.raccolta dati
 //creo la matrice
-    
+const team = [
+    {fullName: 'Wayne Barnett' , job: 'Founder & CEO' , imgMember: 'wayne-barnett-founder-ceo.jpg'},
+    {fullName: 'Angela Caroll' , job: 'Chief Editor' , imgMember: 'angela-caroll-chief-editor.jpg'},
+    {fullName: 'Walter Gordon' , job: 'Office Manager' , imgMember: 'walter-gordon-office-manager.jpg'},
+    {fullName: 'Angela Lopez' , job: 'Social Media Manager' , imgMember: 'angela-lopez-social-media-manager.jpg'},
+    {fullName: 'Scott Estrada' , job: 'Developer' , imgMember: 'scott-estrada-developer.jpg'},
+    {fullName: 'Barbara Ramos' , job: 'Graphic Designer' , imgMember: 'barbara-ramos-graphic-designer.jpg'},
+]
+console.table(team)    
 // 3.lavorazione dati
 
     
